2adcb168463be19ded11fbad7dc548a5fdb6a4b8
Abdul Al Roman <mdroman45678910@gmail.com>
2025-09-11 00:13:14 +0600

    add some metadata

--- src/components/Navber.jsx
+++ src/components/Navber.jsx
@@ -104,7 +104,7 @@ export default function Navber() {
         {/* Right side */}
         <div className="flex items-center gap-4">
           <div className="flex items-center gap-2">
-            {/* Theme Changer */}
+            {/* Theme Changer button */}
             <ThemeToggler />
             {/* Notification */}
             <NotificationMenu />
